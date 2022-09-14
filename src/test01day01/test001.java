3f736243cc953eefdb93742550068da497c59d02
leeyeonwoo <eto3e40@gmail.com>
2022-09-14 16:45:28 +0900

    this is test

---
+++ src/test01day01/test001.java
@@ -0,0 +1,12 @@
+package test01day01;
+
+public class test001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i < 10; i++) {
+			System.out.println("hello~ world");
+		}
+	}
+
+}
